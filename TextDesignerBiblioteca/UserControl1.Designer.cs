21abded0286148cff4ea6c56d358ad417776c75e
JaimeCuenca <jaime.cuenca.caro@gmail.com>
2021-02-18 12:25:44 +0100

    BIBLIOTECA FINAL

--- TextDesignerBiblioteca/UserControl1.Designer.cs
+++ TextDesignerBiblioteca/UserControl1.Designer.cs
@@ -47,7 +47,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(44, 77);
+            this.checkBox1.Location = new System.Drawing.Point(26, 13);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(47, 17);
             this.checkBox1.TabIndex = 1;
@@ -58,7 +58,7 @@
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(44, 100);
+            this.checkBox2.Location = new System.Drawing.Point(26, 36);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(48, 17);
             this.checkBox2.TabIndex = 2;
@@ -69,7 +69,7 @@
             // checkBox3
             // 
             this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(44, 123);
+            this.checkBox3.Location = new System.Drawing.Point(26, 59);
             this.checkBox3.Name = "checkBox3";
             this.checkBox3.Size = new System.Drawing.Size(75, 17);
             this.checkBox3.TabIndex = 3;
@@ -79,7 +79,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(45, 155);
+            this.button1.Location = new System.Drawing.Point(27, 91);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
@@ -91,7 +91,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(339, 198);
+            this.label1.Location = new System.Drawing.Point(321, 134);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(43, 13);
             this.label1.TabIndex = 5;
@@ -99,7 +99,7 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(45, 191);
+            this.numericUpDown1.Location = new System.Drawing.Point(27, 127);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             65,
             0,
@@ -124,7 +124,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(89, 191);
+            this.label2.Location = new System.Drawing.Point(71, 127);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(51, 13);
             this.label2.TabIndex = 7;
@@ -140,7 +140,7 @@
             "Verdana",
             "Harlow",
             "Aharoni"});
-            this.listBox1.Location = new System.Drawing.Point(44, 227);
+            this.listBox1.Location = new System.Drawing.Point(26, 163);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(66, 30);
             this.listBox1.TabIndex = 9;
@@ -149,7 +149,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(116, 227);
+            this.label4.Location = new System.Drawing.Point(98, 163);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(28, 13);
             this.label4.TabIndex = 10;
@@ -160,7 +160,7 @@
             this.checkBox4.Appearance = System.Windows.Forms.Appearance.Button;
             this.checkBox4.AutoSize = true;
             this.checkBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox4.Location = new System.Drawing.Point(78, 278);
+            this.checkBox4.Location = new System.Drawing.Point(60, 214);
             this.checkBox4.Name = "checkBox4";
             this.checkBox4.Size = new System.Drawing.Size(35, 30);
             this.checkBox4.TabIndex = 11;
@@ -172,7 +172,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(121, 288);
+            this.label5.Location = new System.Drawing.Point(103, 224);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(23, 13);
             this.label5.TabIndex = 12;
@@ -181,7 +181,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(42, 288);
+            this.label6.Location = new System.Drawing.Point(24, 224);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(30, 13);
             this.label6.TabIndex = 13;
@@ -225,5 +225,9 @@
         private System.Windows.Forms.CheckBox checkBox4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        public void setTexto(string texto)
+        {
+            label1.Text = texto;
+        }
     }
 }
